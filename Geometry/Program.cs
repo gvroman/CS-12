e05f2ec4da62451e4a94381449ceeea5f8f21261
Grigory Roman <green_go@list.ru>
2025-01-09 11:54:28 +0300

    Mod3

---
+++ Geometry/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Geometry
+{
+    enum Figures { Romb, Rect, RightTriangle, Circle }
+    struct Fdata
+    {
+        public int x0, y0;
+        public double a, b;
+        public int color;
+        public Figures type;
+    }
+    class Program
+    {
+        static double area(Fdata fd)
+        {
+            double res = 0;
+            switch (fd.type)
+            {
+                case Figures.Romb: res = fd.a*fd.b/2; break;
+                case Figures.Rect: res = fd.a*fd.b; break;
+                case Figures.RightTriangle: res = fd.a*fd.a*Math.Sqrt(3)/4; break;
+                case Figures.Circle: res = Math.PI*fd.a*fd.a; break;
+            }
+            return res;
+        }
+        static void Main(string[] args)
+        {
+                Fdata fd = new Fdata() { a = 4, b = 3, type = Figures.Romb };
+                Console.WriteLine(area(fd));
+            }
+    }
+}
