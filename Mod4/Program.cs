4dde2ddcba6cc390280bb6fa8a4240af0f1c760d
Grigory Roman <green_go@list.ru>
2025-01-10 12:05:32 +0300

    Mod4

---
+++ Mod4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Mod4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = 0;
+            foreach(string arg in args)
+            {
+                Console.WriteLine(arg);
+
+                int num = int.Parse(arg);
+                res += num;
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
